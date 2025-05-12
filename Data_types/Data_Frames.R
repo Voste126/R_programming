bdac033c7feabcb50552e7c380e0a6c5b25180c2
voste126 <steveaustine126@gmail.com>
2025-05-12 13:35:11 +0300

    FEAT: learning R for AI

--- Data_types/Data_Frames.R
+++ Data_types/Data_Frames.R
@@ -17,3 +17,13 @@ ncol(df)
 
 colnames(df)
 # The colnames() function is used to get the names of the columns in a data frame
+
+
+# Return the first two rows with all of the columns:
+df[1:2, ]
+
+# Return the first and third row (with all of the columns); a comma follows the vector so it indicates rows):
+df[c(1,3), ]
+
+# Return the first and third columns (with all of the rows); no comma follows the vector so it indicates columns:
+df[c(1, 3)] 
